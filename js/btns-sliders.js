93cd3fe189d95ab22668e94816f28712d9093d16
Audiosyncrasy <jerm.killer2002@gmail.com>
2018-02-19 09:07:00 -0800

    changed fontawesome version to 4.7.0 so it's compatible with my code

--- js/btns-sliders.js
+++ js/btns-sliders.js
@@ -3,9 +3,9 @@ var getBtn = document.getElementById('playBtn');
 var getIcon = document.getElementById('transportIcon');
 
 getBtn.addEventListener('click', function () {
-    if (getIcon.className === 'fas fa-play') {
-        getIcon.setAttribute('class', 'fas fa-pause');
+    if (getIcon.className === 'fa fa-play') {
+        getIcon.setAttribute('class', 'fa fa-pause');
     } else {
-        getIcon.setAttribute('class', 'fas fa-play');
+        getIcon.setAttribute('class', 'fa fa-play');
     }
 });
